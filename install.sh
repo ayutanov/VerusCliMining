8d8dd5d4a62769158ae603657051deac1cba7029
ayutanov <81326804+ayutanov@users.noreply.github.com>
2023-09-16 18:28:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,23 +5,22 @@ sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git scree
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-mkdir ~/ccminer
-cd ~/ccminer
 GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/Android-Mining/releases?per_page=1" | jq -c '[.[] | del (.body)]')
 GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
 GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .name")
 
 echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 
-wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
-wget https://raw.githubusercontent.com/ayutanov/VerusCliMining/main/config.json -O ~/ccminer/config.json
-chmod +x ~/ccminer/ccminer
+wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer
+wget https://raw.githubusercontent.com/ayutanov/VerusCliMining/main/config.json -O ~/config.json
+chmod +x ~/ccminer
 
-cat << EOF > ~/ccminer/start.sh
+cat << EOF > ~/start.sh
 #!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config.json
+~/ccminer -c ~/config.json
 EOF
 chmod +x start.sh
+chmod +x config.json
 
 echo "setup nearly complete."
 echo "Edit the config with \"nano ~/ccminer/config.json\""
@@ -31,4 +30,4 @@ echo "use \"<CTRL>-x\" to exit and respond with"
 echo "\"y\" on the question to save and \"enter\""
 echo "on the name"
 
-echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
+echo "start the miner with \"./start.sh\"."
